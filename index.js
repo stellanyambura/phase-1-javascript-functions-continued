00d53e494421a827098339856773794f6d451ee2
stellagitonga <stellanyamburagitonga@gmail.com>
2022-11-25 12:21:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return "This Saturday, I want to "+activity+"!"
+}
+function mondayWork(activityy = 'go to the office'){
+    return 'This Monday, I will '+activityy+"."
+}
+function wrapAdjective(adjective){
+    function special(receive){
+        // return `You are ${adjective}${receive}${adjective}!`
+
+        // same as 
+        
+        return `You are `+ adjective + receive + adjective+"!"
+    }
+    return special
+ }
+ wrapAdjective('*')
